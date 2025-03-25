9fd7cf0e860f7846a8d8d3adb1d5d61404d2de37
KillianXR <9756@holbertonstudents.com>
2025-03-25 10:08:02 +0100

    0-constants.js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,8 +1,5 @@
 export function taskFirst() {
-	var task = 'I prefer const when I can.';
-	{
-		const task = 'I prefer const when I can?';
-	}
+	const task = 'I prefer const when I can?';
 	return task;
 }
 
@@ -11,10 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-	var combination = 'But sometimes let';
-	{
-		let task = 'But sometimes let'
-	}
+	let combination = 'But sometimes let';
 	combination += getLast();
 
 	return combination;
