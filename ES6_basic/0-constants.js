dfafe03144822c6938dcc1001de604625970ea31
KillianXR <9756@holbertonstudents.com>
2025-03-25 10:03:19 +0100

    js first commit

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,21 @@
+export function taskFirst() {
+	var task = 'I prefer const when I can.';
+	{
+		const task = 'I prefer const when I can?';
+	}
+	return task;
+}
+
+export function getLast() {
+	return ' is okay';
+}
+
+export function taskNext() {
+	var combination = 'But sometimes let';
+	{
+		let task = 'But sometimes let'
+	}
+	combination += getLast();
+
+	return combination;
+}
